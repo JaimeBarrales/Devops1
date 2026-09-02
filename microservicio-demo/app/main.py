ac91394204ed5ff140d29722d03d0412a15a9075
JaimeBarrales <jai.barrales@duocuc.cl>
2026-09-02 18:28:55 -0400

    estrucutura inicial

---
+++ microservicio-demo/app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"mensaje": "Microservicio activo", "status": "OK"}
+
+@app.get("/health")
+def health_check():
+    return {"status": "healthy"}
+
+@app.get("/saludo/{nombre}")
+def saludar(nombre: str):
+    return {"mensaje": f"Hola, {nombre}!"}
+
+@app.get("/suma/{a}/{b}")
+def sumar (a: int, b: int):
+    return {"resultado": a + b}
---
+++ microservicio-demo/tests/test_main.py
@@ -0,0 +1,20 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+
+def test_health_check():
+    response = client.get("/health")
+    assert response.json() == {"status": "healthy"}
+
+def test_saludo():
+    response = client.get("/saludo/Ana")
+    assert "Ana" in response.json()["mensaje"]
+
+def test_suma():
+    response = client.get("/suma/3/4")
+    assert response.json()["resultado"] == 7
